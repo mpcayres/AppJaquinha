4c2221a5a0d37a0153793ab17364373b39336ac0
ingridsl <inus2agome@gmail.com>
2017-10-25 21:08:27 -0200

    Tela de Login e Restrições - imagem de Jaca é provisoria

---
+++ app/src/main/java/ihc/appjaquinha/RestricoesActivity.java
@@ -0,0 +1,13 @@
+package ihc.appjaquinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestricoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restricoes);
+    }
+}
